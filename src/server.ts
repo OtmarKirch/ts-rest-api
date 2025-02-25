6d0021e1d8e8d86bcadbba326f04e3797aaddf08
OtmarKirch <154664034+OtmarKirch@users.noreply.github.com>
2025-02-25 19:24:18 +0100

    Configure port

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,29 @@
 import * as express from 'express';
 import { root } from './routes/root';
+import * as utils from './utils';
 
 const app = express();
 
 function setupExpress() {
-
-    
     app.route("/").get(root);
-
-
 }
 
 function startServer() {
+    console.log('Process arguments:', process.argv);
+    const portArg = process.argv[2];
+    let port;
 
-    app.listen(9000, ()=> {
-        console.log("v2 HTTP Server running at http://localhost:9000");
-    })
+    if (utils.isInteger(portArg)) {
+        port = parseInt(portArg);
+    } else {
+        port = 9000; // Default port if the argument is not a valid integer
+    }
 
+    console.log(`Starting server on port ${port}...`);
+
+    app.listen(port, () => {
+        console.log(`v2 HTTP Server running at http://localhost:${port}`);
+    });
 }
 
 setupExpress();
