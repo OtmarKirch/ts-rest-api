1af4d7c18d7b92b59f164cda764b765ba5551c93
OtmarKirch <154664034+OtmarKirch@users.noreply.github.com>
2025-03-01 14:11:05 +0100

    Add error handling to getAllCourses

--- src/routes/get-all-courses.ts
+++ src/routes/get-all-courses.ts
@@ -1,11 +1,17 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { logger } from '../logger';
 import { AppDataSource } from '../datasource';
 import { Course } from '../models/course';
 
-export async function getAllCourses(request: Request, response: Response) {
-    
-    logger.debug("Getting all courses...");
+export async function getAllCourses(
+    request: Request,
+    response: Response,
+    next: NextFunction) {
+
+    try {
+        logger.debug("Getting all courses...");
+
+    throw {error: "Thrown ERROR"}
 
     const courses = await AppDataSource
         .getRepository(Course)
@@ -14,4 +20,9 @@ export async function getAllCourses(request: Request, response: Response) {
         .getMany();
 
     response.status(200).json({courses})
+    } catch (error) {
+        logger.error("Error in getAllCourses: ", error);
+        next(error)
+    }
+    
 }
